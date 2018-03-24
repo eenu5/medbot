2db111387d38fe9194c66b3c03ddd705d4f7289c
mohak1 <30952926+mohak1@users.noreply.github.com>
2018-03-24 13:24:18 +0530

    Added more methods
    methods to get more data from the user

--- TakingDetails.py
+++ TakingDetails.py
@@ -6,6 +6,7 @@ import numpy as np
 import json
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
+import random
 
 
 #greeting file
@@ -62,15 +63,15 @@ def getName(text):
     #takes the user response and returns name of the user
     filtered = stopWords(text)
     stemmed = stemming(filtered)
-    print("stemmed",stemmed)
+##    print("stemmed",stemmed)
     tag = nltk.pos_tag(stemmed)
     #print(tag)
     noun=[]
     for i in range(len(tag)):
-        print(tag[i][1])
-        if str(tag[i][1])=='NN':
-            noun.append(i)
-    print(noun)
+##        print(tag[i][1])
+        if ((str(tag[i][1])=='NN' or str(tag[i][1])=='NNP') and str(tag[i][0])!='name'):
+            noun.append(tag[i][0])
+##    print(noun)
 ##    chunkGram = r"""Chunk: {<NN+>*}  """
 ##    chunkParser = nltk.RegexpParser(chunkGram)
 ##    chunked = chunkParser.parse(tag)
@@ -81,34 +82,83 @@ def getName(text):
 ##            print('i=',i[0])
 ##
 ##    print(name[0])
-    return 
-    
-
-        
-    
-            
+    return noun
 
 def greet():
-    k = np.random.randint(50)
+    k = random.randint(0,50)
     print(gd[k%11])
 
 def askName():
-    k = np.random.randint(50)
+    k = random.randint(0,50)
     print(nd[k%7])
     inp = input()
     return inp
+
+def askAge():
+    k = random.randint(0,50)
+    print(ad[k%8])
+    inp = input()
+    return inp
+
+def getAge(text):
+    #text is a sentence(string)
+    #expected output: age in number
+    filtered = stopWords(text)
+    for i in filtered:
+        try:
+            age = int(i)
+        except Exception as e:
+            continue
+    return age
+
+def askGender():
+    print('Are you a Male or a Female?')
+    inp = input()
+    return inp
+
+def sorry():
+    print('I\'m sorry I could not understand that. Let\'s try again.')
+
+def getGender(text):
+    #text is a sentence(string)
+    #expected output: 'Male' or 'Female'
+    filtered = stopWords(text)
+    for i in filtered:
+        if i.lower()=='male' or i.lower()=='female':
+            gender = i
+            flag=1
+    if flag!=1:
+        return 0
+    else:
+        return gender
+
+def getEmail():
     
+
+
 #Starting the conversation 
 greet()
 print('I\'m MedBot, your personal health assistant.')
-print("I can help you find out what's going on with a symptom simple assisment.")
-ufText = askName()
-name = getName(ufText)
+print("I can help you find out what's going on with a simple symptom assisment.")
+ufName = askName()
+name = getName(ufName)
+ufAge = askAge()
+age = getAge(ufAge)
+ufGender = askGender()
+gender = getGender(ufGender)
+while gender==0:
+    sorry()
+    ufGender = askGender()
+    gender = getGender(ufGender)
+print('To help you keep a record of your symptoms and enable us to provide you with better assistance, we would like you to provide us with your email. This is mandatory.')
+email = getEmail()
 
+##print('name = {}, age = {}'.format(name[0],age))
+print('Okay {} '.format(name[0]))
+print('Can you please discribe ')
 
 ##while(True):
     #because the user might type things continuously
     #return when the user says something to stop the conversation
 
-    #get a random number for greeting(size = 0-7)
-    
+    #get a random number for greeting(size = 0-7)      
